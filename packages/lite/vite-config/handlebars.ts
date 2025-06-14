26279e6bb9b47af2ea245dfe60322424ca1df167
Equal Ma <equalma@outlook.com>
2025-06-14 10:54:23 +0800

    chore: handlebars plugin filter id

--- packages/lite/vite-config/handlebars.ts
+++ packages/lite/vite-config/handlebars.ts
@@ -4,26 +4,29 @@ const { precompile } = handlebarsImport;
 
 const handlebars: Plugin = {
   name: "handlebars",
-  transform(code, id) {
-    if (!id.endsWith(".hbs")) {
-      return null;
-    }
+  transform: {
+    filter: { id: /\.hbs$/ },
+    handler(code, id) {
+      if (!id.endsWith(".hbs")) {
+        return null;
+      }
 
-    const { code: spec, map } = precompile(code, {
-      srcName: id,
-      strict: true,
-      preventIndent: true,
-    }) as { code: string; map: string };
+      const { code: spec, map } = precompile(code, {
+        srcName: id,
+        strict: true,
+        preventIndent: true,
+      }) as { code: string; map: string };
 
-    if (typeof spec !== "string") {
-      throw new Error("unexpected template spec", spec);
-    }
+      if (typeof spec !== "string") {
+        throw new Error("unexpected template spec", spec);
+      }
 
-    // TODO: the sourcemap is wrong because we add some code
-    return {
-      code: `export default ${spec};`,
-      map,
-    };
+      // TODO: the sourcemap is wrong because we add some code
+      return {
+        code: `export default ${spec};`,
+        map,
+      };
+    },
   },
 };
 
